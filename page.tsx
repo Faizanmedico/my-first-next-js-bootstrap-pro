09b8134e63d50dadf6879d5dbe7898bb580cf1fe
Sultan Ahmed <146020547+Faizanmedico@users.noreply.github.com>
2025-01-28 19:03:44 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import About from './about/page'
+
+const Home = () => {
+  return (
+    <div>
+      <h1>HOME</h1>
+    <About />
+      
+      </div>
+  )
+}
+
+export default Home
